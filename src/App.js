5009428890d7492683af18ec64a2dbebbcf52e14
Marc Francisco <marcpfrancisco@gmail.com>
2020-05-06 17:25:19 +0800

    [ADDED] Heading Template

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Layout from "./components/Layout";
-import About from "./pages/About";
-import Blog from "./pages/Blog";
-import Works from "./pages/Works";
+
+import Heading from "./pages/Heading";
 import "./App";
 
 export class App extends Component {
@@ -14,11 +13,7 @@ export class App extends Component {
 				<React.Fragment>
 					<NavBar />
 					<Layout>
-						<Switch>
-							<Route path="/about" component={About} />
-							<Route path="/blog" component={Blog} />
-							<Route path="/works" component={Works} />
-						</Switch>
+						<Heading />
 					</Layout>
 				</React.Fragment>
 			</Router>
--- src/pages/Heading.js
+++ src/pages/Heading.js
@@ -2,8 +2,18 @@ import React from "react";
 
 const Heading = () => {
 	return (
-		<div>
-			<h1>Heading Works!</h1>
+		<div className="heading">
+			<p className="hello-there">Hello there! I am,</p>
+			<div className="heading-details">
+				<h1>Marc Piolo Francisco</h1>
+				<h3 className="grey">think twice, code once.</h3>
+			</div>
+			<div className="heading-info">
+				<p className="heading-info_text">
+					I’m a Software Engineer, based in Quezon City, PHL. Specialized in
+					Frontend Development but also can work on Backend and Mobile Stuff. ❤️
+				</p>
+			</div>
 		</div>
 	);
 };
